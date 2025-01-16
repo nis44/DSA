e9d20f5426534cc9a9067ebc5ab5737d78887418
nis44 <155174386+nis44@users.noreply.github.com>
2025-01-16 20:51:19 +0530

    Time: 0 ms (100.00%) | Memory: 70.2 MB (44.53%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.cpp
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.cpp
@@ -1,36 +1,21 @@
 class Solution {
 public:
-
-    bool isVowel(char ch) {
-        return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
-    }
-
-    bool check(const string& str) {
-        return isVowel(str.front()) && isVowel(str.back());
-    }
-
     vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
         int n = words.size();
-        vector<int> ans(n, 0);
-        if(check(words[0])) ans[0] = 1;
-        for(int i = 1; i < n; i++) {
-            ans[i] = ans[i - 1] + (check(words[i]) ? 1 : 0);
+        vector<int> Prefix(n + 1, 0);
+        unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u'}; 
+        for (int i = 0; i < n; i++) {
+            Prefix[i + 1] = Prefix[i];
+            if (vowels.count(words[i].front()) && vowels.count(words[i].back())) {
+                Prefix[i + 1]++;  
+            }
+        }
+        vector<int> ANS; 
+        for (auto& query : queries) {
+            int L = query[0], R = query[1];  
+            ANS.push_back(Prefix[R + 1] - Prefix[L]);  
         }
 
-        
-        n = queries.size();
-        vector<int> res(n);
-
-        for(int i = 0; i < n; i++) {
-            res[i] = ans[queries[i][1]] - ans[queries[i][0]];
-            if(check(words[queries[i][0]])) res[i]++;
-        }
-
-        for(auto it : ans) cout<< it<<" ";
-
-        return res;
-
-
-
+        return ANS;  
     }
 };
