8eba2ecc681528fb5823acaeb150064182de674d
nis44 <155174386+nis44@users.noreply.github.com>
2025-02-06 19:22:10 +0530

    Time: 375 ms (20.69%) | Memory: 85.1 MB (23.20%) - LeetSync

--- 1364-tuple-with-same-product/tuple-with-same-product.cpp
+++ 1364-tuple-with-same-product/tuple-with-same-product.cpp
@@ -3,22 +3,19 @@ public:
     int tupleSameProduct(vector<int>& nums) {
         int n = nums.size();
         unordered_map<int, int> mpp;
+
         for(int i = 0; i < n; i++) {
-            for(int j = i+1; j < n; j++) {
+            for(int j = i + 1; j < n; j++) { 
                 mpp[nums[i] * nums[j]]++;
             }
         }
 
         int ans = 0;
-        int cnt = 0;
         for(auto it: mpp) {
-            if(it.second >= 2) {
-                cnt = 0;
-                cnt += (it.second * (it.second - 1)) / 2;
-                cnt *= 8;
-                ans += cnt;
+            int freq = it.second;
+            if(freq >= 2) {
+                ans += (freq * (freq - 1)) / 2 * 8;
             }
-            
         }
         return ans;
     }
