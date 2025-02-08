cf8530b44c189845bc4f4b991beeada642aa4ec1
nis44 <155174386+nis44@users.noreply.github.com>
2025-02-09 00:34:27 +0530

    Time: 16 ms (22.00%) | Memory: 31.8 MB (68.64%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,24 +1,4 @@
 class Solution {
-
-private:
-    void dfs(int row, int col, vector<vector<int>>& vis,
-             vector<vector<int>>& grid, int delrow[], int delcol[]) {
-                int m = grid.size();
-                int n = grid[0].size();
-
-        vis[row][col] = 1;
-
-        for (int i = 0; i < 4; i++) {
-            int nrow = row + delrow[i];
-            int ncol = col + delcol[i];
-
-            if (nrow >= 0 && nrow < m && ncol >= 0 && ncol < n &&
-                !vis[nrow][ncol] && grid[nrow][ncol] == 1) {
-                dfs(nrow, ncol, vis, grid, delrow, delcol);
-            }
-        }
-    }
-
 public:
     int numEnclaves(vector<vector<int>>& grid) {
         int m = grid.size();
@@ -27,39 +7,58 @@ public:
         vector<vector<int>> vis(m, vector<int>(n, 0));
         queue<pair<int, int>> q;
 
+        // Traverse first and last row
+        for (int i = 0; i < n; i++) {
+            if (!vis[0][i] && grid[0][i] == 1) {
+                q.push({0, i});
+                vis[0][i] = 1;  // Mark as visited
+            }
+            if (!vis[m - 1][i] && grid[m - 1][i] == 1) {
+                q.push({m - 1, i});
+                vis[m - 1][i] = 1;  // Mark as visited
+            }
+        }
+
+        // Traverse first and last column
+        for (int i = 0; i < m; i++) {
+            if (!vis[i][0] && grid[i][0] == 1) {
+                q.push({i, 0});
+                vis[i][0] = 1;  // Mark as visited
+            }
+            if (!vis[i][n - 1] && grid[i][n - 1] == 1) {
+                q.push({i, n - 1});
+                vis[i][n - 1] = 1;  // Mark as visited
+            }
+        }
+
         int delrow[] = {-1, 0, 1, 0};
         int delcol[] = {0, 1, 0, -1};
 
-        for (int i = 0; i < n; i++) {
-            if (!vis[0][i] && grid[0][i] == 1) {
-                dfs(0, i, vis, grid, delrow, delcol);
-            }
+        // BFS traversal
+        while (!q.empty()) {
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
 
-            if (!vis[m - 1][i] && grid[m - 1][i] == 1) {
-                dfs(m-1, i, vis, grid, delrow, delcol);
-            }
-        }
+            for (int i = 0; i < 4; i++) {
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
 
-        for (int i = 0; i < m; i++) {
-            if (!vis[i][0] && grid[i][0] == 1) {
-                dfs(i, 0, vis, grid, delrow, delcol);
-            }
-
-            if (!vis[i][n - 1] && grid[i][n - 1] == 1) {
-                dfs(i, n-1, vis, grid, delrow, delcol);
+                if (nrow >= 0 && nrow < m && ncol >= 0 && ncol < n &&
+                    !vis[nrow][ncol] && grid[nrow][ncol] == 1) {
+                    q.push({nrow, ncol});
+                    vis[nrow][ncol] = 1;  // Mark as visited when pushing
+                }
             }
         }
 
-        
-
-       
-
+        // Count enclaves
         int cnt = 0;
-
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1 && !vis[i][j])
+                if (grid[i][j] == 1 && !vis[i][j]) {
                     cnt++;
+                }
             }
         }
 
