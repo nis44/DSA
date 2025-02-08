5e54fd5f8b15f58d2f1893bd4cc36f6e03a5dac7
nis44 <155174386+nis44@users.noreply.github.com>
2025-02-08 16:00:38 +0530

    Time: 111 ms (67.50%) | Memory: 195.8 MB (71.56%) - LeetSync

--- 2434-design-a-number-container-system/design-a-number-container-system.cpp
+++ 2434-design-a-number-container-system/design-a-number-container-system.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 class NumberContainers {
 private:
-    unordered_map<int, int> indexToValue;  // {index -> number}
-    unordered_map<int, set<int>> valueToIndices;  // {number -> sorted set of indices}
+    unordered_map<int, int> indexToValue;  
+    unordered_map<int, set<int>> valueToIndices;  
 
 public:
     NumberContainers() {
@@ -12,26 +12,21 @@ public:
     }
     
     void change(int index, int number) {
-        // If index already has a value, remove it from the old value's set
         if (indexToValue.find(index) != indexToValue.end()) {
             int oldValue = indexToValue[index];
             valueToIndices[oldValue].erase(index);
 
-            // If no indices left for oldValue, remove entry
             if (valueToIndices[oldValue].empty()) {
                 valueToIndices.erase(oldValue);
             }
         }
-
-        // Update mappings
         indexToValue[index] = number;
         valueToIndices[number].insert(index);
     }
     
     int find(int number) {
-        // If number exists in valueToIndices, return the smallest index
         if (valueToIndices.find(number) != valueToIndices.end() && !valueToIndices[number].empty()) {
-            return *valueToIndices[number].begin();  // Get the smallest index
+            return *valueToIndices[number].begin();  
         }
         return -1;
     }
