6ec6a377e12c9f597700adc38d4dbd7e15daa5b1
nis44 <155174386+nis44@users.noreply.github.com>
2025-05-27 01:09:05 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (33.06%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -7,14 +7,16 @@ public:
 
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(i == 0 && j == 0) dp[i][j] = 1;
-                else {
-                    int up = 0; int right = 0;
+
+                if(i == 0 && j == 0) continue;
+
+                int up = 0; int right = 0;
+
+
 
                     if(i > 0) up = dp[i-1][j];
                     if(j > 0) right = dp[i][j-1];
                     dp[i][j] = up + right;
-                }
             }
         }
 
