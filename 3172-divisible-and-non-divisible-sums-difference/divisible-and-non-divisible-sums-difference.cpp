242bc0f0a6c69915bd3b1560c98d79655c05caeb
nis44 <155174386+nis44@users.noreply.github.com>
2025-05-27 16:59:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (36.04%) - LeetSync

--- 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.cpp
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int differenceOfSums(int n, int m) {
-        int ans = 0;
-        for(int i = 1; i <= n; i++) {
-            if(i % m == 0) ans -= i;
-            else {
-                ans += i;
-                cout<<i<<" ";
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {  
+            if (i % m == 0) {
+                sum -= i;
+            } else {
+                sum += i;
             }
         }
-        return ans;
+        return sum;
     }
 };
