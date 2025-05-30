d8a39e2e1242ce84bc1dfc2e1630eb74bab07f61
nis44 <155174386+nis44@users.noreply.github.com>
2025-05-31 02:38:29 +0530

    Time: 1771 ms (13.26%) | Memory: 9.2 MB (93.26%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
+
+    bool isPali(int i, int j, string &s) {
+        while(i <= j) {
+            if(s[i] != s[j]) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
     int minCut(string s) {
         int n = s.size();
+
         vector<int> dp(n + 1, 0);
-        vector<vector<bool>> isPal(n, vector<bool>(n, false));
 
-        // Precompute palindrome substrings
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = i; j < n; j++) {
-                if (s[i] == s[j] && (j - i <= 1 || isPal[i + 1][j - 1])) {
-                    isPal[i][j] = true;
-                }
-            }
-        }
-
-        // Bottom-up DP to compute min cuts
-        for (int i = n - 1; i >= 0; i--) {
+        for(int i = n-1; i >= 0; i--) {
             int mini = INT_MAX;
-            for (int j = i; j < n; j++) {
-                if (isPal[i][j]) {
+            for(int j = i; j < n; j++) {
+                if(isPali(i, j, s)) {
                     int cost = 1 + dp[j + 1];
                     mini = min(mini, cost);
                 }
