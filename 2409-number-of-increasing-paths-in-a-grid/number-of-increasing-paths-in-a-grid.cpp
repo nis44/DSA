36b54d4f6752e193ae9e99e1ed3099f4ff57781a
nis44 <155174386+nis44@users.noreply.github.com>
2025-06-16 00:16:34 +0530

    Time: 59 ms (41.10%) | Memory: 47.9 MB (58.39%) - LeetSync

--- 2409-number-of-increasing-paths-in-a-grid/number-of-increasing-paths-in-a-grid.cpp
+++ 2409-number-of-increasing-paths-in-a-grid/number-of-increasing-paths-in-a-grid.cpp
@@ -1,35 +1,43 @@
 class Solution {
+    
     int mod = 1e9 + 7;
     int dirs[4][2] = {{0,1},{1,0},{-1,0},{0,-1}};
-public:
-    int dfs(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp) {
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        int m = grid.size(), n = grid[0].size();
-        long res = 1;
 
-        for(auto& d : dirs) {
-            int ni = i + d[0], nj = j + d[1];
-            if(ni >= 0 && ni < m && nj >= 0 && nj < n && grid[ni][nj] > grid[i][j]) {
-                res += dfs(ni, nj, grid, dp);
-                res %= mod;
+private:
+
+    int dfs(vector<vector<int>> &grid, vector<vector<int>>&dp,  int row, int col) {
+        if(dp[row][col] != -1) return dp[row][col];
+
+        int m = grid.size();
+        int n = grid[0].size();
+
+        int res = 1;
+
+        for(auto &it: dirs) {
+            int nrow = row + it[0];
+            int ncol = col + it[1];
+
+            if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && grid[nrow][ncol] > grid[row][col]) {
+                res = (res + dfs(grid, dp, nrow, ncol)) % mod;
             }
         }
 
-        return dp[i][j] = res;
+        return dp[row][col] = res;
     }
 
+public:
     int countPaths(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        long total = 0;
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int> (n, -1));
 
-        for(int i = 0; i < m; ++i)
-            for(int j = 0; j < n; ++j) {
-                total += dfs(i, j, grid, dp);
-                total %= mod;
+        long long ans = 0;
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                ans = (ans + dfs(grid, dp, i, j)) % mod;
             }
-
-        return total;
+        }
+        return ans;
     }
 };
