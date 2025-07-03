2962b108519661506cf8a03d114b6e5745770b69
nis44 <155174386+nis44@users.noreply.github.com>
2025-07-03 22:39:04 +0530

    Time: 10 ms (57.38%) | Memory: 11.4 MB (66.36%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,40 +1,37 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-       unordered_map<char, int> mpp;
+        unordered_map<char, int> mpp;
         for(int i = 0; i < t.size(); i++) {
             mpp[t[i]]++;
         }
 
-        int l = 0, r = 0, cnt = 0;
-        int mini = 1e9;
-        int str = -1;
+        int l = 0, r = 0, mini = 1e9, cnt = 0;
+        int sidx = -1;
 
         while(r < s.size()) {
             if(mpp[s[r]] > 0) {
                 cnt++;
-                
             }
             mpp[s[r]]--;
-
             while(cnt == t.size()) {
+                // mini = min(mini, r - l + 1);
+                // sidx = l;
                 if(mini > r - l + 1) {
                     mini = r - l + 1;
-                    str = l;
+                    sidx = l;
                 }
-
                 mpp[s[l]]++;
-                
-
                 if(mpp[s[l]] > 0) cnt--;
                 l++;
+                
             }
+
+            
 
             r++;
         }
 
-        return str == -1 ? "" : s.substr(str, mini);
-
-        
+        return sidx == -1 ? "" : s.substr(sidx, mini);
     }
 };
