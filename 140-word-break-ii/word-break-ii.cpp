58aab5bae50e96576d444aa27e1aea8d96416181
nis44 <155174386+nis44@users.noreply.github.com>
2025-07-09 14:00:55 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (42.21%) - LeetSync

--- 140-word-break-ii/word-break-ii.cpp
+++ 140-word-break-ii/word-break-ii.cpp
@@ -1,38 +1,45 @@
 class Solution {
 public:
-    vector<vector<string>> ans;
-    unordered_set<string> st;
-    vector<string> wordBreak(string s, vector<string>& wordDict) {
-        st = unordered_set<string>(wordDict.begin(), wordDict.end());
-        vector<string> temp;
-        solve(0, s, temp);
 
-        vector<string> res;
-        for(auto &vec : ans) {
-            string sentence = "";
-            for(int i = 0; i < vec.size(); i++) {
-                sentence += vec[i];
-                if(i != vec.size() - 1) sentence += " ";
-            }
-            res.push_back(sentence);
-        }
-
-        return res;
-    }
-
-    void solve(int start, string &s, vector<string> &temp) {
-        if(start == s.size()) {
+    void solve(int str, string s, unordered_map<string, int> &mpp, vector<vector<string>> & ans, vector<string> &temp) {
+        if(str == s.size()) {
             ans.push_back(temp);
             return;
         }
 
-        for(int i = start; i < s.size(); i++) {
-            string word = s.substr(start, i - start + 1);
-            if(st.count(word)) {
+
+        for(int i = str; i < s.size(); i++) {
+            string word = s.substr(str, i - str + 1);
+            if(mpp.find(word) != mpp.end()) {
                 temp.push_back(word);
-                solve(i + 1, s, temp);
-                temp.pop_back();  // backtrack
+                solve(i + 1, s, mpp, ans, temp);
+                temp.pop_back();
             }
         }
+    }
+
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        unordered_map<string, int> mpp;
+        for(int i = 0; i < wordDict.size(); i++) {
+            mpp[wordDict[i]]++;
+        }
+        vector<vector<string>> ans;
+        vector<string> temp;
+
+        solve(0, s, mpp, ans, temp);
+
+        vector<string> res;
+        for(auto &vec: ans) {
+            string sen = "";
+            for(int i = 0; i < vec.size(); i++) {
+                sen += vec[i];
+                sen += " ";
+            }
+            sen.pop_back();
+
+            res.push_back(sen);
+        }
+
+        return res;
     }
 };
